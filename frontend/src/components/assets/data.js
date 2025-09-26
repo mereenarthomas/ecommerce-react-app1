3b71c8ce50e6d0ef92f2f36bf97cd2bc20d2ae89
Mereena <127683989+mereenarthomas@users.noreply.github.com>
2025-09-26 15:08:18 +0530

    Add files via upload

---
+++ frontend/src/components/assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Bubble Velvet Jacket",
+    image:p1_img,
+    new_price:1550.00,
+    old_price:2780.50,
+  },
+  {id:2,
+    name:"Basic Square Neck Top",
+    image:p2_img,
+    new_price:580.00,
+    old_price:720.50,
+  },
+  {id:3,
+    name:"Nike Dual Colored Top",
+    image:p3_img,
+    new_price:560.00,
+    old_price:700.50,
+  },
+  {id:4,
+    name:"Flutter Sleeve Overlap Top",
+    image:p4_img,
+    new_price:500.00,
+    old_price:850.00,
+  },
+];
+
+export default data_product;
---
+++ frontend/src/components/assets/new_collections.js
@@ -0,0 +1,69 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Basic Full Sleeve Round Neck Top",
+    image: p1_img,
+    new_price: 450.0,
+    old_price: 580.5,
+  },
+  {
+    id: 35,
+    name: "Colourblocked Hooded Sweatshirt",
+    image: p2_img,
+    new_price: 585.0,
+    old_price: 820.5,
+  },
+  {
+    id: 14,
+    name: "Colourblocked Full-Zip Slim Fit Jacket",
+    image: p3_img,
+    new_price: 880.0,
+    old_price: 1160.5,
+  },
+  {
+    id: 8,
+    name: "Floral Balloon Sleeve Top",
+    image: p4_img,
+    new_price: 510.0,
+    old_price: 750.0,
+  },
+  {
+    id: 15,
+    name: "Printed Zippered Slim Fit Jacket",
+    image: p5_img,
+    new_price: 790.0,
+    old_price: 1030.5,
+  },
+  {
+    id: 2,
+    name: "Basic Square Neck Top",
+    image: p6_img,
+    new_price: 580.0,
+    old_price: 720.5,
+  },
+  {
+    id: 17,
+    name: "Denim Faded Bomber Jacket",
+    image: p7_img,
+    new_price: 920.0,
+    old_price: 1200.5,
+  },
+  {
+    id: 28,
+    name: "Basic Round Neck Sweatshirt",
+    image: p8_img,
+    new_price: 500.0,
+    old_price: 750.0,
+  },
+];
+
+export default new_collections;
